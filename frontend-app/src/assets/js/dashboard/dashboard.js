c1cb7cd19df82c38aad51803490ebcdcbb35ec26
Daniel Brazel <dbrazel4@gmail.com>
2018-10-06 14:11:27 -0400

    Made framework for patient dashboard

--- frontend-app/src/assets/js/dashboard/dashboard.js
+++ frontend-app/src/assets/js/dashboard/dashboard.js
@@ -46,145 +46,6 @@ $(document).ready(function () {
     $('#privacypolicylink').attr('href', config.other.privacyPolicyUrl);
     $('#helplink').attr('href', config.other.helpPageUrl);
 
-    function downloadCSV() {
-        var BOM = "\uFEFF";
-        var datastring = "";
-        firebase.database().ref('locations/' + window.locationSelect).once('value').then(function (location) {
-            var locationId = location.key;
-            //console.log(locationId);
-            var locationData = location.val();
-            var contactIdData = locationData.contacts;
-            if (contactIdData !== undefined) {
-                $("#nodatawarning").addClass("collapse");
-                //console.log(contactIdData);
-                var locationname = locationData.name;
-                var numcontacts = locationData.numcontacts;
-                var numContactIterations = 0;
-                for (var index in contactIdData) {
-                    if (contactIdData.hasOwnProperty(index)) {
-                        //console.log(contactIdData[index]);
-                        var contactId = contactIdData[index].id;
-                        //console.log(contactId);
-                        firebase.database().ref('contacts/' + contactId).once('value').then(function (contact) {
-                            var contactData = contact.val();
-                            //console.log(contactData);
-                            for (var i = 0; i < config.other.outlookfieldnames.length; i++) {
-                                //replace / with - to avoid json key problems
-                                var outlookfieldname = config.other.outlookfieldnames[i].replace('/', '-');
-                                if (contactData.hasOwnProperty(outlookfieldname)) {
-                                    var contactDataValue = contactData[outlookfieldname];
-                                    if (outlookfieldname == "Notes") {
-                                        contactDataValue = "\"" + contactDataValue + "\"";
-                                    }
-                                    datastring = datastring + contactDataValue + ",";
-                                } else {
-                                    datastring = datastring + ",";
-                                }
-                            }
-                            datastring += "\r\n";
-                            numContactIterations++;
-                            if (numcontacts == numContactIterations) {
-                                var csvData = BOM + datastring;
-                                var blob = new Blob([csvData], {
-                                    type: "text/csv;charset=utf-8"
-                                });
-                                var rightnow = new Date();
-                                var datevalues = [
-                                    rightnow.getFullYear(),
-                                    rightnow.getMonth() + 1,
-                                    rightnow.getDate(),
-                                    rightnow.getHours(),
-                                    rightnow.getMinutes(),
-                                    rightnow.getSeconds()
-                                ];
-                                var filename = locationname + '-' + datevalues[0] + '-' + ("0" + datevalues[1]).slice(-2) + '-' + ("0" + datevalues[2]).slice(-2) + '-' + ("0" + datevalues[3]).slice(-2) + '-' + ("0" + datevalues[4]).slice(-2) + '.csv';
-                                var dateTime = Date.now();
-                                firebase.database().ref('locations/' + locationId).update({
-                                    lastdownloaddate: dateTime,
-                                    numcontacts: 0
-                                }).then(function () {
-                                    firebase.database().ref('locations/' + locationId + '/contacts').remove().then(function () {
-                                        FileSaver.saveAs(blob, filename);
-                                    }).catch(function (error) {
-                                        handleError(error);
-                                    });
-                                }).catch(function (error) {
-                                    handleError(error);
-                                });
-                            }
-                        }).catch(function (error) {
-                            handleError(error);
-                        });
-                    }
-                }
-            } else {
-                //console.log("contact id data undefined");
-                $("#nodatawarning").removeClass("collapse");
-            }
-        }).catch(function (error) {
-            handleError(error);
-        });
-    }
-
-    function createDownloadButton() {
-        createdDownloadButton = true;
-        $("#downloadLocationCSV").on('click touchstart', function () {
-            downloadCSV();
-        });
-    }
-
-    function createlocationSelect() {
-        //console.log("Create location select");
-        firebase.database().ref('locations').once('value').then(function (locations) {
-            var locationSelectString = "<select class=\"selectpicker locationSelect\" data-live-search=\"true\" id=\"locationSelect\">" +
-                "<option data-tokens=\"none\" value=\"\">none</option>";
-            var numlocations = locations.numChildren();
-            var countlocations = 0;
-            locations.forEach(function (location) {
-                countlocations++;
-                var locationVal = location.val();
-                var loctionId = location.key;
-                //console.log("location data: " + locationVal, loctionId);
-                var locationName = locationVal.name;
-                //console.log(locationName);
-                locationSelectString = locationSelectString + "<option data-tokens=\"" + locationName + "\" value=\"" + loctionId + "\">" + locationName + "</option>";
-                if (countlocations == numlocations) {
-                    locationSelectString = locationSelectString + "</select><br/><br/><br/><br/>";
-                    $('#locationSelect').selectpicker('destroy');
-                    $('#downloadSelectCollapse').append(locationSelectString);
-                    $('#locationSelect').selectpicker();
-                    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
-                        $('#locationSelect').selectpicker('mobile');
-                    }
-                    $('.selectpicker').selectpicker();
-                    //console.log("location select: " + locationSelectString);
-                    $('.locationSelect').on("change", function (elem) {
-                        $("#nodatawarning").addClass("collapse");
-                        // on select update locationSelect variable
-                        //console.log($(this));
-                        //console.log("changed value");
-                        var locationSelect = elem.target.value;
-                        //console.log("selected " + locationSelect);
-                        //console.log(locationSelect);
-                        if (locationSelect !== "") {
-                            window.locationSelect = locationSelect;
-                            $('#downloadButtonCollapse').removeClass("collapse");
-                            if (!createdDownloadButton) {
-                                createDownloadButton();
-                            }
-                        } else {
-                            window.locationSelect = "";
-                            $('#downloadButtonCollapse').addClass("collapse");
-                        }
-                    });
-                    $("#downloadSelectCollapse").removeClass("collapse");
-                }
-            });
-        }).catch(function (error) {
-            handleError(error);
-        });
-    }
-
     function createContactSubmitForm() {
         if ($("#addContactForm").valid()) {
             //console.log("form valid");
@@ -267,7 +128,7 @@ $(document).ready(function () {
             },
             ""
         );
-    
+
         $("#addContactForm").validate({
             rules: {
                 firstname: {
@@ -308,7 +169,7 @@ $(document).ready(function () {
             window.email = user.email;
             var testemail = new RegExp(config.regex.adminemailregex, 'g');
             $("#bodycollapse").removeClass("collapse");
-            $("#addContactCollapse").removeClass("collapse");
+            $("#patientDash").removeClass("collapse");
             createValidation();
             $("#addContactSubmit").on('click touchstart', function () {
                 createContactSubmitForm();
