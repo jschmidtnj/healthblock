579226c7eabe398f0ba131a0c2446bdba9893131
Joshua Schmidt <jschmidtnj@gmail.com>
2018-10-07 15:49:01 -0400

    logout work

--- frontend-app/src/assets/js/dashboard/dashboard.js
+++ frontend-app/src/assets/js/dashboard/dashboard.js
@@ -45,46 +45,41 @@ $(document).ready(function () {
     $('#privacypolicylink').attr('href', config.other.privacyPolicyUrl);
     $('#helplink').attr('href', config.other.helpPageUrl);
 
-
-
     var signed_in_initially = false;
 
     firebase.auth().onAuthStateChanged(function (user) {
-        window.user = user
-        var ref = firebase.database().ref('users/' + user.uid);
-      ref.once('value').then(function(snapshot) {
-          console.log("snapshot", snapshot.val().usertype)
-        var usertype = snapshot.val().usertype;
-
-        if (usertype == "patient") { 
-          console.log("Patient signed in");
-            // User is signed in.
-            //console.log("signed in");
-            $("#bodycollapse").removeClass("collapse");
-            $("#patientDash").removeClass("collapse");
+        if (user) {
+            window.user = user
             signed_in_initially = true;
-            firebase.database().ref("requests/").once('value').then(function(snapshot){
-                snapshot.forEach(function(childSnapshot){
-                    if (childSnapshot.val().patientID == window.user.uid){
-                        console.log(childSnapshot.val())
-                        var doctor = childSnapshot.val().doctor;
-                        $("#notificationsBody").append(
-                            "<tr><td>" + doctor + "</td><td><button id='approve1' class='btn btn-primary'>Accept</button></td><td><button id='reject1' class='btn btn-primary'>Reject</button></td></tr>"
-                        )
-                    }
-                })
-
-               
-                
-            })
-            
-        } else if (usertype == "doctor"){ 
-          window.email = user.email;
-          var testemail = new RegExp(config.regex.adminemailregex, 'g');
-          $("#bodycollapse").removeClass("collapse");
-          $("#doctorDash").removeClass("collapse");
-          signed_in_initially = true;
-        } /*else {
+            var ref = firebase.database().ref('users/' + user.uid);
+            ref.once('value').then(function (snapshot) {
+                console.log("snapshot", snapshot.val().usertype)
+                var usertype = snapshot.val().usertype;
+                if (usertype == "patient") {
+                    console.log("Patient signed in");
+                    // User is signed in.
+                    //console.log("signed in");
+                    $("#bodycollapse").removeClass("collapse");
+                    $("#patientDash").removeClass("collapse");
+                    firebase.database().ref("requests/").once('value').then(function (snapshot) {
+                        snapshot.forEach(function (childSnapshot) {
+                            if (childSnapshot.val().patientID == window.user.uid) {
+                                console.log(childSnapshot.val())
+                                var doctor = childSnapshot.val().doctor;
+                                $("#notificationsBody").append(
+                                    "<tr><td>" + doctor + "</td><td><button id='approve1' class='btn btn-primary'>Accept</button></td><td><button id='reject1' class='btn btn-primary'>Reject</button></td></tr>"
+                                )
+                            }
+                        })
+                    })
+                } else if (usertype == "doctor") {
+                    window.email = user.email;
+                    var testemail = new RegExp(config.regex.adminemailregex, 'g');
+                    $("#bodycollapse").removeClass("collapse");
+                    $("#doctorDash").removeClass("collapse");
+                }
+            });
+        } else {
             // No user is signed in. redirect to login page:
             if (signed_in_initially) {
                 $('#alertsignoutsuccess').fadeIn();
@@ -108,68 +103,67 @@ $(document).ready(function () {
                 //fast redirect
                 // window.location.href = 'login.html';
             }
-        } */
-      });
+        }
     });
 
-    $("#logoutButton").on('click touchstart', function () {
-        firebase.auth().signOut().then(function () {
-            // Sign-out successful.
-        }).catch(function (error) {
-            // An error happened.
-            handleError(error);
-        });
+$("#logoutButton").on('click touchstart', function () {
+    firebase.auth().signOut().then(function () {
+        // Sign-out successful.
+    }).catch(function (error) {
+        // An error happened.
+        handleError(error);
     });
+});
 
 //events for approve and reject buttons
-    $("#notifications").on("click", "#approve1", function(event){
-        event.preventDefault();
-        var parent = $(event.target.hash).parent()
-        console.log("approve")
-    })
+$("#notifications").on("click", "#approve1", function (event) {
+    event.preventDefault();
+    var parent = $(event.target.hash).parent()
+    console.log("approve")
+})
 
-    $("#notifications").on("click", "#reject1", function(event){
-        event.preventDefault();
-        var parent = $(event.target).parent()
-        console.log("reject")
-    })
+$("#notifications").on("click", "#reject1", function (event) {
+    event.preventDefault();
+    var parent = $(event.target).parent()
+    console.log("reject")
+})
 
 //event for doctor request data
-    $("#submitDataRequest").on("click", function(event){
-        event.preventDefault();
-        var patientID = $("#getPatientData").serializeArray()[0].value;
-        firebase.database().ref('users/' + patientID).once('value').then(function(snapshot){
-            var patient = snapshot.val();
-            console.log("patient", patient);
-            console.log(window.user.uid);
-            firebase.database().ref('users/' + window.user.uid).once('value').then(function(snapshot1){
-                var doctorName = snapshot1.val().name;
-                console.log("patientID", patientID);
-                var requests = firebase.database().ref('requests/');
-                var sendRequests = requests.push({
-                    patientID: patientID,
-                    doctor: doctorName
-                });
-                sendRequests;
-                console.log("request sent");
-            })
+$("#submitDataRequest").on("click", function (event) {
+    event.preventDefault();
+    var patientID = $("#getPatientData").serializeArray()[0].value;
+    firebase.database().ref('users/' + patientID).once('value').then(function (snapshot) {
+        var patient = snapshot.val();
+        console.log("patient", patient);
+        console.log(window.user.uid);
+        firebase.database().ref('users/' + window.user.uid).once('value').then(function (snapshot1) {
+            var doctorName = snapshot1.val().name;
+            console.log("patientID", patientID);
+            var requests = firebase.database().ref('requests/');
+            var sendRequests = requests.push({
+                patientID: patientID,
+                doctor: doctorName
+            });
+            sendRequests;
+            console.log("request sent");
         })
-    });
-//submit ehr form
-    $("#submitEhr").on("click", function(event){
-        event.preventDefault();
-        var ehrData = $("#ehrForm").serializeArray()
-        console.log(ehrData);
-        var ehr = firebase.database().ref('ehr/' + ehrData[0].value);
-        var sendEhr = ehr.push({
-            name: ehrData[1].value,
-            age: ehrData[2].value,
-            prescription: ehrData[3].value,
-            diagnosis: ehrData[4].value,
-            notes: ehrData[5].value,
-            other: ehrData[6].value
-        })
-        sendEhr;
-        console.log(sendEhr, "ehr sent")
     })
+});
+//submit ehr form
+$("#submitEhr").on("click", function (event) {
+    event.preventDefault();
+    var ehrData = $("#ehrForm").serializeArray()
+    console.log(ehrData);
+    var ehr = firebase.database().ref('ehr/' + ehrData[0].value);
+    var sendEhr = ehr.push({
+        name: ehrData[1].value,
+        age: ehrData[2].value,
+        prescription: ehrData[3].value,
+        diagnosis: ehrData[4].value,
+        notes: ehrData[5].value,
+        other: ehrData[6].value
+    })
+    sendEhr;
+    console.log(sendEhr, "ehr sent")
+})
 });
